7ce91e3c43218986be8926f2a0582adb110a00f4
김동현 <dhkim960511@naver.com>
2023-10-26 16:48:56 +0900

    study msa

---
+++ msa_practice/msademo/src/test/java/com/kdh/msademo/MsademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdh.msademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
