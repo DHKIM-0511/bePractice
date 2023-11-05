8ed5b38106c5dcf359b97a4186f4edf6f940f14a
김동현 <dhkim960511@naver.com>
2023-11-05 21:12:11 +0900

    practice test

---
+++ testing/cafekiosk/src/test/java/com/example/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafekiosk;
+
+import com.example.cafekiosk.unit.CafeKiosk;
+import com.example.cafekiosk.unit.beverage.Americano;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+
+
+}
