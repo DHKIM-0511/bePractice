105c4f1a1033e3a0f0b74fe1aea0e997ebaf8550
loophyun <dhkim960511@naver.com>
2025-10-22 21:36:31 +0900

    브리지 패턴 적용

---
+++ design-patterns/src/main/java/me/nrz/design_patterns/_02_structural_patterns/_07_bridge/_01_before/Champion.java
@@ -0,0 +1,18 @@
+package me.nrz.design_patterns._02_structural_patterns._07_bridge._01_before;
+
+
+import me.nrz.design_patterns._02_structural_patterns._07_bridge._02_after.Skin;
+
+public interface Champion extends Skin {
+
+    void move();
+
+    void skillQ();
+
+    void skillW();
+
+    void skillE();
+
+    void skillR();
+
+}
