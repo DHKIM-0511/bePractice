7eda8833ad8d47fdf2bca9f77e0a13851fc92e9d
loophyun <dhkim960511@naver.com>
2025-10-27 22:19:13 +0900

    컴포짓 패턴 적용

---
+++ design-patterns/src/main/java/me/nrz/design_patterns/_02_structural_patterns/_08_composite/_01_before/Client.java
@@ -0,0 +1,31 @@
+package me.nrz.design_patterns._02_structural_patterns._08_composite._01_before;
+
+/**
+ * printPrice(Item item), printPrice(Bag bag) 같은 메서드를 모두 클라이언트가 알아야하나?
+ *
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Item doranBlade = new Item("도란검", 450);
+        Item healPotion = new Item("체력 물약", 50);
+
+        Bag bag = new Bag();
+        bag.add(doranBlade);
+        bag.add(healPotion);
+
+        Client client = new Client();
+        client.printPrice(doranBlade);
+        client.printPrice(bag);
+    }
+
+    private void printPrice(Item item) {
+        System.out.println(item.getPrice());
+    }
+
+    private void printPrice(Bag bag) {
+        int sum = bag.getItems().stream().mapToInt(Item::getPrice).sum();
+        System.out.println(sum);
+    }
+
+}
---
+++ design-patterns/src/main/java/me/nrz/design_patterns/_02_structural_patterns/_08_composite/_02_after/Component.java
@@ -0,0 +1,10 @@
+package me.nrz.design_patterns._02_structural_patterns._08_composite._02_after;
+
+/**
+ * 클라이언트에서 공통으로 사용하는 operation을 Interface로 빼는게 중요함
+ */
+public interface Component {
+
+    int getPrice();
+
+}
