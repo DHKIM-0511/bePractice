aeff698c6b9e469338754ccb3925872bab176825
loophyun <dhkim960511@naver.com>
2025-11-05 21:15:11 +0900

    책임 연쇄 패턴 적용

---
+++ design-patterns/src/main/java/me/nrz/design_patterns/_03_behavioral_patterns/_13_chain_of_responsibilities/_02_after/PrintRequestHandler.java
@@ -0,0 +1,17 @@
+package me.nrz.design_patterns._03_behavioral_patterns._13_chain_of_responsibilities._02_after;
+
+
+import me.nrz.design_patterns._03_behavioral_patterns._13_chain_of_responsibilities._01_before.Request;
+
+public class PrintRequestHandler extends RequestHandler {
+
+    public PrintRequestHandler(RequestHandler nextHandler) {
+        super(nextHandler);
+    }
+
+    @Override
+    public void handle(Request request) {
+        System.out.println(request.getBody());
+        super.handle(request);
+    }
+}
