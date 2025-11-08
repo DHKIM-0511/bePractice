9b83228c8a67898e6a5db682622e4d46990f5d66
loophyun <dhkim960511@naver.com>
2025-11-08 15:30:58 +0900

    커맨드 패턴 적용

---
+++ design-patterns/src/main/java/me/nrz/design_patterns/_03_behavioral_patterns/_14_command/_02_after/GameEndCommand.java
@@ -0,0 +1,23 @@
+package me.nrz.design_patterns._03_behavioral_patterns._14_command._02_after;
+
+
+import me.nrz.design_patterns._03_behavioral_patterns._14_command._01_before.Game;
+
+public class GameEndCommand implements Command {
+
+    private Game game;
+
+    public GameEndCommand(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void execute() {
+        game.end();
+    }
+
+    @Override
+    public void undo() {
+        new GameStartCommand(this.game).execute();
+    }
+}
