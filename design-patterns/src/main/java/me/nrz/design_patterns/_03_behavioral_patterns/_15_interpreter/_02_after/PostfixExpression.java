50203d7a3eaa5def6976db54894ffaecdd49a352
loophyun <dhkim960511@naver.com>
2025-11-08 17:25:22 +0900

    인터프리터 패턴 적용

---
+++ design-patterns/src/main/java/me/nrz/design_patterns/_03_behavioral_patterns/_15_interpreter/_02_after/PostfixExpression.java
@@ -0,0 +1,9 @@
+package me.nrz.design_patterns._03_behavioral_patterns._15_interpreter._02_after;
+
+import java.util.Map;
+
+public interface PostfixExpression {
+
+    int interpret(Map<Character, Integer> context);
+
+}
